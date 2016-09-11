0296e408a03ebfc11e0e9b86bdc46a22226661dc
Ryan Macdonald <ryan.macdonald@shopify.com>
2016-09-10 21:06:12 -0400

    start writing some tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,33 @@
 var assert = require('assert');
+var fs = require('fs');
+var join = require('path').join;
+var pkg = require('../package.json');
+var themekitPath = process.platform === 'win32' ? join(__dirname, '..', 'bin', 'theme.exe') : join(__dirname, '..', 'bin', 'theme');
+var themekit = require('..');
 
-suite('This will test node-themekit', function() {
-  test('It should do something', function() {
-    assert.ok(true, "It's good");
+describe('Theme Kit install', function() {
+  beforeEach(function(done) {
+    themekit.install(function() {
+      done();
+    });
+  });
+
+  it('reinstalls binary', function(done) {
+    var originalCTime = fs.statSync(themekitPath).ctime.getTime(); // eslint-disable-line no-sync
+
+    themekit.install(function() {
+      var newCTime = fs.statSync(themekitPath).ctime.getTime(); // eslint-disable-line no-sync
+      assert.notEqual(originalCTime, newCTime, 'created at times are not equal');
+      done();
+    });
+  });
+
+  it('matches package and binary version', function(done) {
+    themekit.command({
+      args: ['version'],
+    }, function() {
+      assert.equal(pkg.version, newCTime, 'created at times are not equal');
+      done();
+    });
   });
 });
