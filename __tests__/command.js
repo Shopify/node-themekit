86cf67a8a989874e99c93279fbcf39e75f4f804b
Erick Zhao <erick.zhao@mail.mcgill.ca>
2018-08-19 00:55:03 -0400

    Test for parameter mutation

--- __tests__/command.js
+++ __tests__/command.js
@@ -7,13 +7,32 @@ jest.mock('child_process');
 
 describe(('command'), () => {
   test('passes correct args to child process', async () => {
+    // arrange
     const params = {args: ['version']};
+    const args = params.args.concat(['--no-update-notifier']);
+
+    // act
     const {spawn} = require('child_process');
     await themekit.command(params);
+
+    // assert
     expect(spawn).toBeCalledWith(
       path.join(cfg.destination, cfg.binName),
-      params.args,
+      args,
       {cwd: expect.any(String)},
     );
   });
+
+  test('does not mutate input param', async () => {
+    // arrange
+    const params = {args: ['version']};
+    const duplicate = JSON.parse(JSON.stringify(params));
+
+    // act
+    await themekit.command(params);
+
+    // assert
+    expect(params).toMatchObject(duplicate);
+  });
 });
+
