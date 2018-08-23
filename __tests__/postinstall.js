6e876125aba7bd578cb530cd05a55c53f016e4e4
Erick Zhao <erick.zhao@mail.mcgill.ca>
2018-08-23 10:22:27 -0400

    Fix status bar regression by reverting  to process.stdout.write

--- __tests__/postinstall.js
+++ __tests__/postinstall.js
@@ -2,10 +2,7 @@ const themekit = require('../lib/themekit');
 const {version} = require('../lib/config');
 
 test('successfully runs binary', async () => {
-  // arrange
-  global.console.log = jest.fn();
-  // act
+  global.process.stdout.write = jest.fn();
   await themekit.command('version');
-  // assert
-  expect(global.console.log).toHaveBeenLastCalledWith(expect.stringContaining(version));
+  expect(global.process.stdout.write).toHaveBeenLastCalledWith(expect.stringContaining(version));
 });
